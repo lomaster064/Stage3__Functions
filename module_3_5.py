b732464f1b94aae1deed35c922469cef481bbfe9
lomaster064 <64ilya64@gmail.com>
2024-09-17 14:56:54 +0400

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -4,8 +4,10 @@ def get_multiplied_digits(number):
 
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
+    elif first == 0:
+        return 1
     else:
         return first
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(420)
 print(result)
